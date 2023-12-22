3dbebfb9ee1f1a0c13e2dbc9a10c5ae42ce847fb
wooyong99 <wulovesk@naver.com>
2023-12-22 15:58:19 +0900

    feat : 채팅방 중복 검사

--- src/main/java/org/example/ServiceThread.java
+++ src/main/java/org/example/ServiceThread.java
@@ -3,13 +3,16 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ServiceThread implements Runnable {
     Socket child;          //클라이언트와 통신하기 위한 소켓
-
     InputStream is;        //클라이언트와 연결된 입력 스트림 저장
     ObjectInputStream ois; //클라이언트로부터 데이터를 전송받기 위한 스트림
+    OutputStream os;
+    ObjectOutputStream oos;
     String receiveData;    //클라이언트로부터 전송받은 데이터를 저장하기 위한 변수
 
     public ServiceThread(Socket s) {
@@ -18,6 +21,8 @@ public class ServiceThread implements Runnable {
             System.out.println(child.getInetAddress() + "로 부터 연결요청 받음");
             is = child.getInputStream();
             ois = new ObjectInputStream(is);
+            os = child.getOutputStream();
+            oos = new ObjectOutputStream(os);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,9 +38,18 @@ public class ServiceThread implements Runnable {
             while (true) {
                 receiveData = (String) ois.readObject();
                 System.out.println(child.getInetAddress() + "의 메시지:" + receiveData);
-//                if (RoomManager.validateThread(receiveData)) {
-//
-//                }
+                try {
+                    RoomManager.duplicateThread(receiveData);
+
+                } catch (IllegalArgumentException e) {
+                    oos.writeObject(e.getMessage());
+                    oos.flush();
+                    continue;
+                }
+                Thread.currentThread().setName(receiveData);
+                RoomManager.addThread(Thread.currentThread());
+                oos.writeObject(receiveData + "채팅방이 생성되었습니다.");
+                oos.flush();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,6 +58,8 @@ public class ServiceThread implements Runnable {
             try {
                 is.close();
                 ois.close();
+                os.close();
+                oos.close();
                 child.close();
             } catch (IOException e2) {
                 e2.printStackTrace();
